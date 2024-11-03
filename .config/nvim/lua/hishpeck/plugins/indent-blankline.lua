a41e33a74dcbaeda00cb6fd1d09b2649995f5f45
Adrian Castillo <adr.cas97@gmail.com>
2024-11-03 15:29:01 +0100

    Fix the error thrown by indent-blankline.nvim

--- .config/nvim/lua/hishpeck/plugins/indent-blankline.lua
+++ .config/nvim/lua/hishpeck/plugins/indent-blankline.lua
@@ -1,8 +1,9 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+	},
 	event = { "BufReadPre", "BufNewFile" },
 	main = "ibl",
-	opts = {
-		indent = { char = "┊" },
-	},
+	opts = {},
 }

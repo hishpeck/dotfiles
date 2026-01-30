5489cae203abac519689fe0e7dd2c2daf0c4c55f
Adrian Castillo <adr.cas97@gmail.com>
2026-01-30 23:34:32 +0100

    Bring back COSMIC

--- hosts/ac-zenbook-2022/default.nix
+++ hosts/ac-zenbook-2022/default.nix
@@ -5,8 +5,8 @@
     ./hardware-configuration.nix
     ../../modules/system/default.nix
     ../../modules/system/laptop.nix
-    # ../../modules/desktop/cosmic.nix
-    ../../modules/desktop/hyprland.nix
+    ../../modules/desktop/cosmic.nix
+    # ../../modules/desktop/hyprland.nix
   ];
 
   networking.hostName = "ac-zenbook-2022";
--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -19,7 +19,7 @@
 
   environment.systemPackages = with pkgs; [
     kitty       
-    rofi-wayland 
+    rofi
     waybar      
     dunst      
     swww      

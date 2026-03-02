307c17691522aa4b53d9305a250fb78499b919cb
Adrian Castillo <adr.cas97@gmail.com>
2026-03-03 00:14:21 +0100

    Mount RPi5 on local machines

--- hosts/ac-main-pc/default.nix
+++ hosts/ac-main-pc/default.nix
@@ -25,4 +25,26 @@
   security.pki.certificateFiles = [ ./la.crt ./cnc.crt ];
 
   system.stateVersion = "24.05";
+
+  programs.fuse.userAllowOther = true;
+
+  environment.systemPackages = with pkgs; [ sshfs ];
+
+  fileSystems."/home/ac/Pi5" = {
+    device = "ac@rpi5.local:/";
+    fsType = "fuse.sshfs";
+    options = [
+      "x-systemd.automount"
+      "noauto"
+      "x-systemd.idle-timeout=600"
+      "_netdev"
+
+      "allow_other"
+      "reconnect"
+      "ServerAliveInterval=15"
+      "StrictHostKeyChecking=accept-new"
+
+      "IdentityFile=/home/ac/.ssh/id_zenbook"
+    ];
+  };
 }

43a4e23b837792666bfb50e9b3376769f029738b
Adrian Castillo <adr.cas97@gmail.com>
2026-03-31 10:49:37 +0200

    Update grub config

--- modules/system/default.nix
+++ modules/system/default.nix
@@ -86,4 +86,14 @@
     home-manager
     nps
   ];
+
+  boot.loader.systemd-boot.enable = false;
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  
+  boot.loader.grub.theme = pkgs.catppuccin-grub;
+
+  boot.loader.grub.useOSProber = true;
 }

4c0e034b5304d9f7fe26ce97d6b426c99895fc9a
Adrian Castillo <adr.cas97@gmail.com>
2026-06-01 11:06:58 +0200

    Add UpNote

---
+++ custom-apps/upnote/default.nix
@@ -0,0 +1,32 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  version = "9.18.11";
+  pname = "upnote";
+
+  src = pkgs.fetchurl {
+    url = "https://download.getupnote.com/app/UpNote.AppImage";
+    sha256 = "0fymmkb1af3d8k4hbx8a1ww24wfixal78ga5ggvc6yd6qi8pvvcy";
+  };
+
+  appimageContents = pkgs.appimageTools.extract { inherit pname version src; };
+
+in pkgs.appimageTools.wrapType2 {
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    install -m 444 -D ${appimageContents}/upnote.desktop \
+      $out/share/applications/${pname}.desktop
+
+    for size in 16 32 48 64 128 256 512 1024; do
+      icon="${appimageContents}/usr/share/icons/hicolor/''${size}x''${size}/apps/upnote.png"
+      if [ -f "$icon" ]; then
+        install -m 444 -D "$icon" \
+          "$out/share/icons/hicolor/''${size}x''${size}/apps/${pname}.png"
+      fi
+    done
+
+    substituteInPlace $out/share/applications/${pname}.desktop \
+      --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${pname} --no-sandbox %U'
+  '';
+}

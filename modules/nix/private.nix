8a66bdbc2860e4683db548ad87104e24501b0826
Adrian Castillo <adr.cas97@gmail.com>
2026-06-29 12:14:26 +0200

    Rollback to SSHFS for now

--- modules/nix/private.nix
+++ modules/nix/private.nix
@@ -1,19 +1,28 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  # Zapewnia wsparcie klienta NFS na poziomie jądra NixOS
-  boot.supportedFilesystems = [ "nfs" ];
+  programs.fuse.userAllowOther = true;
 
-  fileSystems."/home/ac/Pi5/mnt/external2" = {
-    device = "rpi5.local:/mnt/external2";
-    fsType = "nfs";
+  environment.systemPackages = with pkgs; [ sshfs ];
+
+  fileSystems."/home/ac/Pi5" = {
+    device = "ac@rpi5.local:/";
+    fsType = "fuse.sshfs";
     options = [
       "x-systemd.automount"
       "noauto"
       "x-systemd.idle-timeout=600"
       "x-systemd.mount-timeout=5"
       "_netdev"
-      "nfsvers=4.2"
+
+      "allow_other"
+      "reconnect"
+      "ServerAliveInterval=15"
+      "ServerAliveCountMax=3"
+      "StrictHostKeyChecking=accept-new"
+
+      "ConnectTimeout=5"
+      "IdentityFile=/home/ac/.ssh/id_zenbook"
     ];
   };
 }
